cca3c8c44dd4e441bf8be49a52dfebb6b75b8cc4
SFGrenade <sfgrenade@gmail.com>
2018-07-26 02:07:00 +0200

    Update Script

--- 1jux_user_blocker.user.js
+++ 1jux_user_blocker.user.js
@@ -1,10 +1,16 @@
 // ==UserScript==
 // @name         1Jux User Blocker
 // @namespace    https://1jux.net
-// @version      0.1
+// @version      0.1.1
 // @description  Hide posts based on a list of usernames
 // @author       SFGrenade
+
 // @match        https?://(de|en).1jux.net/*
+
+// @homepage    https://github.com/SFGrenade/1Jux-User-Blocker
+// @downloadURL https://github.com/SFGrenade/1Jux-User-Blocker/raw/master/1jux_user_blocker.user.js
+// @updateURL   https://github.com/SFGrenade/1Jux-User-Blocker/raw/master/1jux_user_blocker.user.js
+
 // @grant        GM_info
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -12,24 +18,38 @@
 // @run-at       document-end
 // ==/UserScript==
 
-var blocked_users = [
-    "TestUser123123123123"
-];
+const VERSION = GM_info.script.version;
+const AUTHOR = "SFGrenade";
+
+const string_delimer = " !,! ";
+var blocked_users = GM_getValue("BLOCKED_USERS", "");
 
 function arrayContains(needle, arrhaystack) {
     return (arrhaystack.indexOf(needle) > -1);
 }
 
-window.hide = function() {
-    var posts = document.getElementsByClassName("post-item");
-
-    for (var i = 0; i < posts.length; i++) {
-        var username = posts[i].children[1].firstChild.firstChild.children[2].firstChild.text;
-        if (arrayContains(username, blocked_users)) {
-            posts[i].style.display = "none";
-        }
-    }
+window.onload = () => {
+    // First is the under categories, second one is under settings, third one is under profile
+    drop_down_menu = document.getElementsByClassName("drop-link-list")[2];
+    drop_down_menu.innerHTML += "<a class=\"drop-link \" href=\"#\"><i class=\"fa fa-ban\"></i> Blocked Users</a>";
 };
 
-window.addEventListener("scroll", window.hide, false);
+window.hide = function() {
+    console.log("Test Hide");
+//    var posts = document.getElementsByClassName("post-item");
+
+//    for (var i = 0; i < posts.length; i++) {
+//        // post-item -> l-post-box -> l-post-head-container -> l-post-head -> l-jux-down-tags -> first <a> tag -> text
+//        var post_box = posts[i].children[1];
+//        var username = post_box.firstChild.firstChild.children[2].getElementsByTagName("a")[0].text;
+//        var post_images = post_box.getElementsByTagName("a");
+//        if (arrayContains(username, blocked_users)) {
+//            post_images.forEach(function(entry) {
+//                entry.style.display = "none";
+//            });
+//        }
+//    }
+};
+
+window.addEventListener("JUX.ajax.es", window.hide, false);
 window.hide()
