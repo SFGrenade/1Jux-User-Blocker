ad3a3d01f689f6ddd144c1404e9da6b07bab0ba3
SFGrenade <sfgrenade@gmail.com>
2018-07-26 02:27:02 +0200

    Testing with site stuff

--- 1jux_user_blocker.user.js
+++ 1jux_user_blocker.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         1Jux User Blocker
 // @namespace    https://1jux.net
-// @version      0.1.1
+// @version      0.1.2
 // @description  Hide posts based on a list of usernames
 // @author       SFGrenade
 
-// @match        https?://(de|en).1jux.net/*
+// @match        https://de.1jux.net/*
+// @match        https://en.1jux.net/*
+// @match        http://de.1jux.net/*
+// @match        http://en.1jux.net/*
 
 // @homepage    https://github.com/SFGrenade/1Jux-User-Blocker
 // @downloadURL https://github.com/SFGrenade/1Jux-User-Blocker/raw/master/1jux_user_blocker.user.js
@@ -30,26 +33,32 @@ function arrayContains(needle, arrhaystack) {
 
 window.onload = () => {
     // First is the under categories, second one is under settings, third one is under profile
-    drop_down_menu = document.getElementsByClassName("drop-link-list")[2];
+    var drop_down_menu = document.getElementsByClassName("drop-link-list")[2];
     drop_down_menu.innerHTML += "<a class=\"drop-link \" href=\"#\"><i class=\"fa fa-ban\"></i> Blocked Users</a>";
 };
 
-window.hide = function() {
-    console.log("Test Hide");
-//    var posts = document.getElementsByClassName("post-item");
+function show_blocked_users() {
+    var ajax_window = document.getElementById("ajax");
+    ajax_window.style.color = "red";
+    JUX.ajax.fill();
+}
 
-//    for (var i = 0; i < posts.length; i++) {
-//        // post-item -> l-post-box -> l-post-head-container -> l-post-head -> l-jux-down-tags -> first <a> tag -> text
-//        var post_box = posts[i].children[1];
-//        var username = post_box.firstChild.firstChild.children[2].getElementsByTagName("a")[0].text;
-//        var post_images = post_box.getElementsByTagName("a");
-//        if (arrayContains(username, blocked_users)) {
-//            post_images.forEach(function(entry) {
-//                entry.style.display = "none";
-//            });
-//        }
-//    }
+function update_page() {
+    var posts = document.getElementsByClassName("post-item");
+
+    for (var i = 0; i < posts.length; i++) {
+        // post-item -> l-post-box -> l-post-head-container -> l-post-head -> l-jux-down-tags -> first <a> tag -> text
+        var post_box = posts[i].children[1];
+        var username = post_box.firstChild.firstChild.children[2].getElementsByTagName("a")[0].text;
+        var post_images = post_box.getElementsByTagName("a");
+        if (arrayContains(username, blocked_users)) {
+            post_images.forEach(function(entry) {
+                entry.style.display = "none";
+            });
+        }
+    }
 };
 
-window.addEventListener("JUX.ajax.es", window.hide, false);
-window.hide()
+
+window.addEventListener("scroll", update_page, false);
+update_page()
